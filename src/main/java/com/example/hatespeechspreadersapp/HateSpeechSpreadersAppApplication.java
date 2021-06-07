64903bab59041e4f3eb0009ac04c44cccf93d6d7
Emre <emre.binnaz@ceng.deu.edu.tr>
2021-06-07 23:10:39 +0300

    Initial commit

---
+++ src/main/java/com/example/hatespeechspreadersapp/HateSpeechSpreadersAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.hatespeechspreadersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateSpeechSpreadersAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HateSpeechSpreadersAppApplication.class, args);
+		System.out.println("emre");
+	}
+
+}
---
+++ src/test/java/com/example/hatespeechspreadersapp/HateSpeechSpreadersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hatespeechspreadersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateSpeechSpreadersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
